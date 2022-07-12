99179b25cf08f841fdcaa6343d2ada5dda2e8968
이다영 <ldy1552v@gmail.com>
2022-07-12 17:12:05 +0900

    2022.07.12 이다영
    부가요금 수정 기능 완성(권한, 메인에서 클릭 이동 제외 [나중에 한번에 처리] )

--- com-project-clean/src/main/java/com/project/clean/controller/pay/PayController.java
+++ com-project-clean/src/main/java/com/project/clean/controller/pay/PayController.java
@@ -25,13 +25,21 @@ public class PayController {
 		this.payService = payService;
 	}
 	
-	/* 관리자 급여 전체조회 */
+	/* 직원 급여 전체조회 */
 	@GetMapping("/employeePaySelect")
 	public void employeePaySelect() {}
 	
-	/* 관리자 급여 상세조회 */
+	/* 직원 급여 상세조회 */
 	@GetMapping("/employeePaySelectInfo")
 	public void employeePaySelectInfo() {}
+	
+	/* 관리자 급여 전체조회 */
+	@GetMapping("/adminPaySelect")
+	public void adminPaySelect() {}
+	
+	/* 관리자 급여 상세조회 */
+	@GetMapping("/adminPaySelectInfo")
+	public void adminPaySelectInfo() {}
 	
 	/* 부가요금 페이지 */
 	@GetMapping("/surcharge")
@@ -46,17 +54,62 @@ public class PayController {
 	}
 	
 	/* 부가요금 수정 */
-//	@PostMapping("/surcharge")
-//	public String modify(RedirectAttributes rttr, @ModelAttribute SurchargeDTO surcharge) {
-//		
-//		System.out.println("여기도 못 오냐?");
-//		payService.modifySurcharge(surcharge);
-//		 
-//		rttr.addFlashAttribute("modifySuccessMessage", "부가요금 수정에 성공하셨습니다");
-//		  
-//		return "redirect:/surcharge/" + surcharge.getSurchargeCommission() + surcharge.getSurchargeBonus();
-//		 
+	@PostMapping("/surcharge")
+	public String modify(RedirectAttributes rttr, SurchargeDTO surcharge) {
+
+		payService.modifySurcharge(surcharge);
+		 
+		rttr.addFlashAttribute("modifySuccessMessage", "부가요금 수정에 성공하셨습니다");
+		  
+		return "redirect:/pay/surcharge";
+		 
+
+	}
+	
+//	@GetMapping("/adminPaySelect")
+//	public ModelAndView searchPage(HttpServletRequest request, ModelAndView mv) {
 //
+//		String currentPage = request.getParameter("currentPage");
+//		int pageNo = 1;
+//
+//		if(currentPage != null && !"".equals(currentPage)) {
+//			pageNo = Integer.parseInt(currentPage);
+//		}
+//
+//		String searchCondition = request.getParameter("searchCondition");
+//		String searchValue = request.getParameter("searchValue");
+//
+//		int totalCount = payService.selectTotalCount(searchCondition, searchValue);
+//
+//		/* 한 페이지에 보여 줄 게시물 수 */
+//		int limit = 10;		//얘도 파라미터로 전달받아도 된다.
+//
+//		/* 한 번에 보여질 페이징 버튼의 갯수 */
+//		int buttonAmount = 5;
+//
+//		/* 페이징 처리를 위한 로직 호출 후 페이징 처리에 관한 정보를 담고 있는 인스턴스를 반환받는다. */
+//		SelectCriteria selectCriteria = null;
+//		if(searchValue != null && !"".equals(searchValue)) {
+//			selectCriteria = Pagenation.getSelectCriteria(pageNo, totalCount, limit, buttonAmount, searchCondition, searchValue);
+//		} else {
+//			selectCriteria = Pagenation.getSelectCriteria(pageNo, totalCount, limit, buttonAmount);
+//		}
+//		System.out.println(selectCriteria);
+//
+//		List<AdminPayAndAdminDTO> adminPayList = payService.searchAdminPayList(selectCriteria);
+//
+//		for(AdminPayAndAdminDTO pay : adminPayList) {
+//			System.out.println(pay);
+//		}
+//
+//		mv.addObject("adminPayList", adminPayList);
+//		mv.addObject("selectCriteria", selectCriteria);
+//		mv.setViewName("pay/adminPaySelect");
+//
+//		return mv;
 //	}
+	
+	
+
 	
 }
