f45fd758e6239ea7c64ca31bd75f6b1aecce1adb
DESKTOP-TPQNTU5\Lenovo <knsol1992@Naver.com>
2022-07-17 15:58:02 +0900

    2022.07.17 나큰솔
    Web-Socket 기반 채팅 기능 구현
    알림으로 수정 예정

---
+++ com-project-clean/src/main/java/com/project/clean/model/dto/commonDTO/ChatMessage.java
@@ -0,0 +1,32 @@
+package com.project.clean.model.dto.commonDTO;
+
+public class ChatMessage {
+
+    private MessageType type;
+    private String content;
+    private String sender;
+
+    public MessageType getType() {
+        return type;
+    }
+
+    public void setType(MessageType type) {
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+}
