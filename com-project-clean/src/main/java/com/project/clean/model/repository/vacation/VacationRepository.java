db52682b5bd2348350ce384d0823eee9ebc31f6b
HANJO94\hanjo <hanjo94@gmail.com>
2022-07-20 01:36:09 +0900

    2022/07/20 01:34 ajax complate about paging function and proceed search part

--- com-project-clean/src/main/java/com/project/clean/model/repository/vacation/VacationRepository.java
+++ com-project-clean/src/main/java/com/project/clean/model/repository/vacation/VacationRepository.java
@@ -35,45 +35,67 @@ public interface VacationRepository extends JpaRepository<Vacation, Integer>{
 	public Page<Vacation> findByVacationFirstConfirmYnOrVacationSecondConfirmYnOrVacationLastConfirmYnOrVacationReturnYn(
 			String firstConfirm, String secondConfirm, String lastConfirm, String returnUn, Pageable pageable);
 
-	@Query(value = "SELECT\r\n"
-			+ "       A.ADMIN_NO\r\n"
-			+ "      ,A.ADMIN_NAME\r\n"
-			+ "      ,B.VACATION_NO\r\n"
-			+ "      ,B.REQUEST_DATE\r\n"
-			+ "      ,B.REQUEST_ADMIN\r\n"
-			+ "      ,B.DRAFTER\r\n"
-			+ "      ,B.VACATION_NAME\r\n"
-			+ "      ,B.VACATION_FIRST_CONFIRM_YN\r\n"
-			+ "      ,B.VACATION_SECOND_CONFIRM_YN\r\n"
-			+ "      ,B.VACATION_LAST_CONFIRM_YN\r\n"
-			+ "      ,B.VACATION_START_DATE\r\n"
-			+ "      ,B.VACATION_END_DATE\r\n"
-			+ "      ,B.VACATION_REASON\r\n"
-			+ "      ,B.VACATION_RETURN_YN\r\n"
-			+ "  FROM TBL_ADMIN A\r\n"
-			+ "  JOIN TBL_VACATION B ON (A.ADMIN_NO = B.ADMIN_NO)\r\n"
-			+ " WHERE A. ADMIN_NAME LIKE '?1'", nativeQuery = true)
-	public Page<Vacation> findByNameContaining(String categoryValue, Pageable pageable);
+	/* 승인 전체 조회 */
+	Page<Vacation> findByVacationLastConfirmYn(String string, Pageable pageable);
+	
+	/* 반려 전체 조회 */
+	Page<Vacation> findByVacationReturnYn(String string, Pageable pageable);
+	
+	/* 전체 이름 검색 */
+	public Page<Vacation> findByRequestAdminContaining(String categoryValue, Pageable pageable);
+	
+	/* 승인 이름 검색*/
+	public Page<Vacation> findByVacationLastConfirmYnAndRequestAdminContaining(String confirm, String categoryValue, Pageable pageable);
+	
+	/* 반려 이름 검색*/
+	public Page<Vacation> findByVacationReturnYnAndRequestAdminContaining(String returnYn, String categoryValue, Pageable pageable);
 
-	@Query(value = "SELECT \r\n"
-			    	+ "       *\r\n"
-				 + "  FROM TBL_VACATION\r\n"
-				 + " WHERE ADMIN_NO = 1\r\n"
-				 + "   AND VACATION_START_DATE BETWEEN ?1 AND ?2", nativeQuery = true)
+	/* 전체 휴가 시작일 검색 */
+	@Query(value = " SELECT \r\n"
+				  + "       *\r\n"
+				  + "  FROM TBL_VACATION\r\n"
+				  + "  WHERE VACATION_START_DATE BETWEEN TO_DATE(?1) AND TO_DATE(?2)", nativeQuery = true)
 	public Page<Vacation> selectBetweenStartDate(String startDate, String endDate, Pageable pageable);
-
-	@Query(value = "SELECT \r\n"
-	    	+ "       *\r\n"
-		 + "  FROM TBL_VACATION\r\n"
-		 + " WHERE ADMIN_NO = 1\r\n"
-		 + "   AND VACATION_END_DATE BETWEEN ?1 AND ?2", nativeQuery = true)
+	/* 전체 휴가 종료일 검색 */
+	@Query(value = " SELECT \r\n"
+			  + "       *\r\n"
+			  + "  FROM TBL_VACATION\r\n"
+			  + "  WHERE VACATION_END_DATE BETWEEN TO_DATE(?1) AND TO_DATE(?2)", nativeQuery = true)
 	public Page<Vacation> selectBetweenEndDate(String startDate, String endDate, Pageable pageable);
+	
+	/* 승인 휴가 시작일 검색 */
+	@Query(value = "  SELECT \r\n"
+			+ "       *\r\n"
+			+ "  FROM TBL_VACATION\r\n"
+			+ "  WHERE VACATION_START_DATE BETWEEN TO_DATE(?1)AND TO_DATE(?2)\r\n"
+			+ "    AND VACATION_LAST_CONFIRM_YN = 'Y'", nativeQuery = true)
+	public Page<Vacation> selectConfirmBetweenStartDate(String startDate, String endDate, Pageable pageable);
+	
+	/* 승인 휴가 종료일 검색 */
+	@Query(value = " SELECT \r\n"
+			+ "       *\r\n"
+			+ "  FROM TBL_VACATION\r\n"
+			+ "  WHERE VACATION_END_DATE BETWEEN TO_DATE(?1)AND TO_DATE(?2)\r\n"
+			+ "    AND VACATION_LAST_CONFIRM_YN = 'Y'", nativeQuery = true)
+	public Page<Vacation> selectConfirmBetweenEndDate(String startDate, String endDate, Pageable pageable);
+	
+	/* 반려 휴가 시작일 검색 */
+	@Query(value = " SELECT \r\n"
+			+ "       *\r\n"
+			+ "  FROM TBL_VACATION\r\n"
+			+ "  WHERE VACATION_START_DATE BETWEEN TO_DATE(?1)AND TO_DATE(?2)\r\n"
+			+ "    AND VACATION_RETURN_YN = 'Y'", nativeQuery = true)
+	public Page<Vacation> selectReturnBetweenStartDate(String startDate, String endDate, Pageable pageable);
+	
+	/* 반려 휴가 종료일 검색 */
+	@Query(value = " SELECT \r\n"
+			+ "       *\r\n"
+			+ "  FROM TBL_VACATION\r\n"
+			+ "  WHERE VACATION_END_DATE BETWEEN TO_DATE(?1)AND TO_DATE(?2)\r\n"
+			+ "    AND VACATION_RETURN_YN = 'Y'", nativeQuery = true)
+	public Page<Vacation> selectReturnBetweenEndDate(String startDate, String endDate, Pageable pageable);
 
-//	public Page<Vacation> findByAdminNoAndRequestDateBetweeen(int adminNo, Date startDate, Date endDate, Pageable pageable);
-//
-//	public Page<Vacation> findByAdminNoAndVacationStartDateBetweeen(int adminNo, Date startDate, Date endDate, Pageable pageable);
-//
-//	public Page<Vacation> findByAdminNoAndVacationEndDateBetweeen(int adminNo, Date startDate, Date endDate, Pageable pageable);
+
 
 
 	
