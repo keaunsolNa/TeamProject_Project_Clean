f6996445ed002ffe1956429e40d879c15b107251
나큰솔이당 <knsol1992@naver.com>
2022-07-26 19:56:48 +0900

    2022.07.26 큰솔
    체크리스트 작성 완료 후 총 업무 누적 시간 추가.

--- com-project-clean/src/main/java/com/project/clean/controller/admin/AdminEmployeeController.java
+++ com-project-clean/src/main/java/com/project/clean/controller/admin/AdminEmployeeController.java
@@ -703,7 +703,7 @@ public class AdminEmployeeController {
 	@GetMapping("/hr/select/selectAllVacation")
 	@ResponseBody
 	public Map<String, Object> selectAllVacationList(String category, String categoryValue, String startDate,
-			String endDate, @PageableDefault(size = maxLine) Pageable pageable) {
+			String endDate, @PageableDefault(direction = Direction.DESC, sort = "vacationNo",size = maxLine) Pageable pageable) {
 		Map<String, Object> map = new HashMap<>();
 		map = adminService.selectAllVacaionList(category, categoryValue, startDate, endDate, pageable);
 
@@ -721,7 +721,7 @@ public class AdminEmployeeController {
 	@GetMapping("/hr/select/selectAllVacationConfirmList")
 	@ResponseBody
 	public Map<String, Object> selectAllVacationConfirmList(String category, String categoryValue, String startDate,
-			String endDate, @PageableDefault(size = maxLine) Pageable pageable) {
+			String endDate, @PageableDefault(direction = Direction.DESC, sort = "vacationNo",size = maxLine) Pageable pageable) {
 		Map<String, Object> map = new HashMap<>();
 		map = adminService.selectAllVacationConfirmList(category, categoryValue, startDate, endDate, pageable);
 
@@ -739,7 +739,7 @@ public class AdminEmployeeController {
 	@GetMapping("/hr/select/selectAllVacationReturnList")
 	@ResponseBody
 	public Map<String, Object> selectAllVacationReturnList(String category, String categoryValue, String startDate,
-			String endDate, @PageableDefault(size = maxLine) Pageable pageable) {
+			String endDate, @PageableDefault(direction = Direction.DESC, sort = "vacationNo",size = maxLine) Pageable pageable) {
 		Map<String, Object> map = new HashMap<>();
 		map = adminService.selectAllVacaionReturnList(category, categoryValue, startDate, endDate, pageable);
 
